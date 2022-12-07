8d70d0d82c65e583470d4bcf073b9cf4abeaae5f
Adi Yuda Pranata <adiyudapranata@gmail.com>
2022-12-08 01:42:49 +0700

    problem1 done

--- problem1/primex.go
+++ problem1/primex.go
@@ -3,25 +3,48 @@ package main
 import "fmt"
 
 func PrimeX(number int) int {
-	var hitungan = 0
+	var bilangan = 1
 	var res = 0
-	for i := 1; i <= number*number; i++ {
-		var habisBagi = 0
-		for j := 1; j <= i; j++ {
-			if i%j == 0 {
-				habisBagi = habisBagi + 1
-			}
+	for number > 0 {
+		if Prime(bilangan) {
+			res = bilangan
+			number--
 		}
-		if habisBagi == 2 {
-			hitungan = hitungan + 1
-			if hitungan == number {
-				res = i
-				break
-			}
-		}
+		bilangan++
 	}
 	return res
 }
+func Prime(number int) bool {
+	if number < 2 {
+		return false
+	}
+	for j := 2; j < number; j++ {
+		if number%j == 0 {
+			return false
+		}
+
+	}
+	return true
+}
+
+// var hitungan = 0
+// var res = 0
+// for i := 1; i <= number*number; i++ {
+// 	var habisBagi = 0
+// 	for j := 1; j <= i; j++ {
+// 		if i%j == 0 {
+// 			habisBagi = habisBagi + 1
+// 		}
+// 	}
+// 	if habisBagi == 2 {
+// 		hitungan = hitungan + 1
+// 		if hitungan == number {
+// 			res = i
+// 			break
+// 		}
+// 	}
+// }
+// return res
 
 func main() {
 	fmt.Println(PrimeX(1))  //2
